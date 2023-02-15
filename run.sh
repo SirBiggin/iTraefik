495a99864102a84e8ba2c1682d7f9cc83de1e679
SirBiggin <Eric.G.Greene@hotmail.com>
2023-02-15 08:56:35 -0700

    a

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /etc/traefik/
+./traefik --configFile=traefik.yml
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+	apache2-utils \
+	net-tools \
+	git
+
+cd /etc/
+git clone https://github.com/SirBiggin/traefik
+cd /etc/traefik/
+sudo mkdir -p /etc/traefik/certs
+sudo touch /etc/traefik/certs/acme.json
+sudo chmod 600 /etc/traefik/certs/acme.json
+cd /etc/traefik/
+ls - l run.sh
+sudo chmod 774 run.sh
+wget https://github.com/traefik/traefik/releases/download/v2.9.7/traefik_v2.9.7_linux_amd64.tar.gz
+tar -zxvf traefik_v2.9.7_linux_amd64.tar.gz
+cd /etc/traefik/
+./run.sh
+
+reboot
